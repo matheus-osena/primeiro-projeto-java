e5c4c9e39376bd28e5ad8e82fb2d2901baf97033
Matheus Sena <matheus_osena@hotmail.com>
2024-03-19 14:38:08 -0300

    first commit

---
+++ priimeiro-programa-java/src/pasta/executavel/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package pasta.executavel;
+
+public class PrimeiraClasse {
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java.");
+		System.out.println("Java é orientado a objeto.");
+	}
+}
